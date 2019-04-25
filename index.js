59a4ef5a750cbc2ffb72623cb62cb79a98fe3527
vitorlimadev <vitorlimadev@outlook.com>
2019-04-24 21:09:15 -0300

    Implementation of Exchange API.

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const path =require('path')
 const convert = require('./lib/convert')
+const apiExchange = require('./lib/api.exchange')
+
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', async (req, res) => {
+    const cotacao = await apiExchange.getCotacao()
+    res.render('home', {
+        cotacao
+    })
 })
 
 app.get('/quotation', (req, res) => {
